ba30306c1f7482db0976d244a58a024e2c120345
islandmakerepic <mysandwich00@gmail.com>
2015-02-15 21:39:30 -0700

    Update Building.lua

--- Building.lua
+++ Building.lua
@@ -2,12 +2,13 @@ _G.create = {
 Building=function(X,Y,Z,Pos)
   coroutine.wrap(function()
   local Build=Instance.new("Model",Workspace)
-  for x=-(X/2),X/2 do
-     for y=1,Y do
-        for z=-(Z/2),Z/2 do
+  for x=-(X/2),X/2,5 do
+     for y=1,Y,5 do
+        for z=-(Z/2),Z/2,5 do
           local part=Instance.new("Part",Build)
           part.Name='Wall'
           part.Size=Vector3.new(5,5,5)
+          part.CFrame=CFrame.new(Pos)*CFrame.new(x,y,z)
           wait(0)
           end
        end
