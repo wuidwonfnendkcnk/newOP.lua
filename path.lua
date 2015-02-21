63882db60c7942a0ee40a8701652c840d237d6b6
islandmakerepic <mysandwich00@gmail.com>
2015-02-20 17:35:32 -0700

    Update path.lua

--- path.lua
+++ path.lua
@@ -10,11 +10,14 @@ local part=Instance.new("Part",char)
 part.FormFactor='Custom'
 part.Transparency=0.5
 part.Size=Vector3.new(2.5,0.1,2.5)
-part.Color = Color3.new(C,0,0)
+part.BrickColor = BrickColor.new(C==0 and 'Really black' or 'Really white')
 part.Anchored=true
+game.Debris:AddItem(part,0.5)
 if KK['q'] then
   part.CanCollide=false
-char['Right Leg'].CFrame=char['Right Leg'].CFrame*CFrame.new(0,-1,0)
+char['Right Leg'].CFrame=char['Right Leg'].CFrame*CFrame.new(0,-0.1,0)
+elseif KK['e'] then
+char['Head'].CFrame=char['Head'].CFrame*CFrame.new(0,0.1,0)
 end
 wait(0)
 part.CFrame = CFrame.new(char.Torso.Position)*CFrame.new(0,-3.3,0)
@@ -24,7 +27,7 @@ end)()
 
 mouse.KeyDown:connect(function(K)
 KK[K]=true
-if KK['e'] then
+if KK['y'] then
 C=C==0 and 1 or 0
 end
 end)
