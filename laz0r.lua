03ae7dc7e8b71346abe2e0e9d31ee3614c0eda89
islandmakerepic <mysandwich00@gmail.com>
2015-02-28 15:07:07 -0700

    Create laz0r.lua

---
+++ laz0r.lua
@@ -0,0 +1,40 @@
+local plr=game.Players.LocalPlayer
+local active=true
+local mouse=plr:GetMouse()
+plr.Chatted:connect(function(m) if m:match'activ' then active=true elseif m:match'inacti' then active=false end end)
+
+mouse.Button1Down:connect(function()
+if not active then return end
+plr.Character.Torso.Anchored=true
+local part=Instance.new("Part",plr.Character)
+part.Size=Vector3.new(10,10,1)
+part.Anchored=true
+part.Transparency=0.3
+part.CFrame=plr.Character.Torso.CFrame*CFrame.new(0,0,-5)
+local p=mouse.Hit.p
+local start=part.CFrame
+local CF=CFrame.new(start.p,p)
+local dist=(start.p-p).magnitude
+if dist>400 then dist=400 end
+local raypart=Instance.new("Part",plr.Character)
+raypart.Size=Vector3.new(2,2,dist)
+raypart.Anchored=true
+raypart.CFrame=CF*CFrame.new(0,0,-dist/2)
+raypart.BrickColor=part.BrickColor
+coroutine.wrap(function()
+for i=1,360,(360/15) do
+wait(0)
+part.CFrame=part.CFrame*CFrame.Angles(0,math.rad(i),0)
+part.BrickColor=BrickColor.Random()
+raypart.BrickColor=part.BrickColor
+for i=1,dist,15 do
+local ex=Instance.new("Explosion",workspace)
+ex.Position = start*CFrame.new(0,0,-(5+dist)).p
+end
+end
+wait()
+game.Debris:AddItem(part,0.1)
+game.Debris:AddItem(raypart,0.2)
+plr.Character.Torso.Anchored=false
+end)()
+end)
