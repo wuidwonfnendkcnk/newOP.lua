9097db146665c07019ea91683d10b75e4c2699c2
islandmakerepic <mysandwich00@gmail.com>
2015-03-01 23:07:09 -0700

    Create fixedmatch.lua

---
+++ fixedmatch.lua
@@ -0,0 +1,16 @@
+_G.match=function(t,ttm)
+local str= t
+local sub=1
+for a=1,#ttm do
+local S=ttm:sub(a,a)
+local fin=false
+for i=sub,#str do
+if not fin then
+fin=true
+if S:lower()==str:lower():sub(i,i) then
+sub=sub+1
+end
+end
+end
+end
+end
