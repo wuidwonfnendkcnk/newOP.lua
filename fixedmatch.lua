28845216e3297f5c87725f6af0df5275db70e25a
islandmakerepic <mysandwich00@gmail.com>
2015-03-01 23:08:26 -0700

    Update fixedmatch.lua

--- fixedmatch.lua
+++ fixedmatch.lua
@@ -2,15 +2,17 @@ _G.match=function(t,ttm)
 local str= t
 local sub=1
 for a=1,#ttm do
-local S=ttm:sub(a,a)
-local fin=false
-for i=sub,#str do
-if not fin then
-fin=true
-if S:lower()==str:lower():sub(i,i) then
-sub=sub+1
+  local S=ttm:sub(a,a)
+  local fin=false
+  if sub>=#str then return true end
+    for i=sub,#str do
+      if not fin then
+        fin=true
+          if S:lower()==str:lower():sub(i,i) then
+            sub=sub+1
+          end
+      end
+    end
 end
-end
-end
-end
+return false
 end
