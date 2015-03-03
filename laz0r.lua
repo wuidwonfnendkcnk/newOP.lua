335c3781f8f7e172c08f26247ef1b283d983a16e
islandmakerepic <mysandwich00@gmail.com>
2015-03-02 19:37:34 -0700

    Update laz0r.lua

--- laz0r.lua
+++ laz0r.lua
@@ -18,13 +18,25 @@ part.CFrame=CFrame.new(start.p,p)*CFrame.new(0,0,-7)
 
 local CF=part.CFrame
 local dist=(start.p-p).magnitude
-if dist>400 then dist=400 end
+if dist>700 then dist=700 end
 local raypart=Instance.new("Part",plr.Character)
 raypart.Size=Vector3.new(2,2,dist)
 raypart.Anchored=true
 raypart.CFrame=CF*CFrame.new(0,0,-dist/2)
 raypart.BrickColor=part.BrickColor
 coroutine.wrap(function()
+  coroutine.wrap(function()
+  local A=1
+local B=5
+local C=15
+for i=1,dist,10 do
+local ex=Instance.new("Explosion",workspace)
+CF=raypart.CFrame*CFrame.new(0,0,dist/2)*CFrame.new(0,0,-(i+5))*CFrame.new(0,0,-5)
+ex.Position = (CF).p
+A=A+1
+if A<B then wait(0) elseif A>=C then A=1 end
+end
+end)()
 for i=1,360,(360/15) do
 wait(0)
 part.CFrame=part.CFrame*CFrame.Angles(0,0,math.rad(i))
@@ -37,16 +49,11 @@ game.Debris:AddItem(part,0.1)
 game.Debris:AddItem(raypart,0.2)
 plr.Character.Torso.Anchored=false
 end)()
-coroutine.wrap(function()
-  local A=1
-local B=5
-local C=15
-for i=1,dist,10 do
-local ex=Instance.new("Explosion",workspace)
-CF=raypart.CFrame*CFrame.new(0,0,dist/2)*CFrame.new(0,0,-(i+5))*CFrame.new(0,0,-5)
-ex.Position = (CF).p
-A=A+1
-if A<B then wait(0) elseif A>=C then A=1 end
-end
-end)()
+coroutine.wrap(funcction()
+  while wait(0) and part and part.Parent~=nil do
+    cff=part.CFrame
+    part.Size=part.Size+Vector3.new(0.05,0.05,0.05)
+    part.CFrame=cff
+    end
+  end)()
 end)
