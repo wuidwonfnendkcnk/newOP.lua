81392a9214f7be1eafb234f3b79450b49e71f007
islandmakerepic <mysandwich00@gmail.com>
2015-03-03 13:52:30 -0700

    Update BOTSNEW.lua

--- BOTSNEW.lua
+++ BOTSNEW.lua
@@ -12,7 +12,7 @@ local Ray=function(CHAR)
 CHAR.Torso.Anchored=true
 r=function() return math.random(-500,500) end
 mouse = {Hit = {p=CFrame.new(r(),math.random(-3,10),r()).p}}
-  if math.random(-100,100)<math.random(-100,100) then
+  if math.random(100)<30 then
     mouse.Hit.p=GetRandom().Torso.CFrame.p
     CHAR.Torso.CFrame=CFrame.new(CHAR.Torso.Position,mouse.Hit.p)
     CHAR.Torso.Anchored=false
@@ -132,7 +132,7 @@ clone.Name=names[math.random(1,#names)]
         end)
         coroutine.wrap(function()
           while wait() do
-            z=math.random(10)
+            z=math.random(6)
             wait(z)
             if math.random(10000)<math.random(1000) and charr then
                         Ray(charr)
@@ -146,7 +146,7 @@ coroutine.wrap(function()
   while wait() do
       local N=names[math.random(1,#names)]
           local NN=N
-      for i=1,11 do
+      for i=1,20 do
           wait()
       local c=clone:clone()
       c.Parent=Workspace
