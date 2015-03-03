e24cc2cff473c697f3080a9aea9f74804b212925
islandmakerepic <mysandwich00@gmail.com>
2015-03-02 18:54:46 -0700

    Update laz0r.lua

--- laz0r.lua
+++ laz0r.lua
@@ -12,7 +12,7 @@ part.Anchored=true
 part.Transparency=0.3
 local p=mouse.Hit.p
 local start=plr.Character.Torso.CFrame*CFrame.new(0,0,-5)
-part.CFrame=CFrame.new(start.p,p)*CFrame.new(0,0,-10)
+part.CFrame=CFrame.new(start.p,p)
 
 
 local CF=CFrame.new(start.p,p)
