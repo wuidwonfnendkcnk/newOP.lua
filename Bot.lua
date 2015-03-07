086fafa43fdf7125dd469c2b6c552e5ddc63230f
islandmakerepic <mysandwich00@gmail.com>
2015-03-07 13:50:00 -0700

    Update Bot.lua

--- Bot.lua
+++ Bot.lua
@@ -35,7 +35,7 @@ end
 local findmatch=function(P,txt)
 for i=1,#Responses do
 txt=txt:lower()
-if txt:match(Responses[i].Main[1]) or txt:match(Responses[i].Main[2]) or txt:match(Responses[i].Main[3])  then
+if txt:match(Responses[i].Main[1]) and txt:match(Responses[i].Main[2]) and txt:match(Responses[i].Main[3])  then
 Chat(Responses[i].Response[math.random(1,#Responses[i].Response)],P)
 end
 end
