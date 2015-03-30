3b00508c866f2b6e478da1fa127d3856f1fba428
islandmakerepic <mysandwich00@gmail.com>
2015-03-29 23:00:39 -0700

    Update flying fight.lua

--- flying fight.lua
+++ flying fight.lua
@@ -1,16 +1,20 @@
 local b=Workspace.Base
 
 fly=function(o)
-v.Velocity=Vector3.new(v.Velocity.X,100,v.Velocity.Z)
+  if o.CFrame.Y-Workspace.Base.Y<100 then
+    o.CFrame=o.CFrame*CFrame.new(0,102,0)
+    end
+o.CFrame=o.CFrame*CFrame.new(0,1,0)
 end
 
 local owat=0
 
 while wait(0) do
 owat=owat+1
+Workspace.Base.CFrame=Workspace.Base.CFrame*CFrame.new(0,1,0)
 if owat>200 then
 owat=0
-wait(5)
+wait(1)
 end
 for i,v in pairs(Workspace:children()) do
 if v.Name~='Base' then
