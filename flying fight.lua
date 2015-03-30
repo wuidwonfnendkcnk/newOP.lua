fc45d2a48bd539499585fed1c339358e27b44151
islandmakerepic <mysandwich00@gmail.com>
2015-03-29 23:07:30 -0700

    Update flying fight.lua

--- flying fight.lua
+++ flying fight.lua
@@ -3,8 +3,10 @@ local b=Workspace.Base
 fly=function(o)
   if o.CFrame.Y-b.Y<100 and o.Name~=b.Name then
     o.CFrame=o.CFrame*CFrame.new(0,102,0)
+    print''
   end
   o.CFrame=o.CFrame*CFrame.new(0,1,0)
+  print'-'
 end
 
 local owat=0
@@ -20,7 +22,6 @@ for i,v in pairs(Workspace:children()) do
 pcall(function() fly(v) end)
 local vv= v:findFirstChild'Torso'
 pcall(function() fly(vv) end)
-pcall(function() v.Humanoid.Jump=true end)
 
 end
 
