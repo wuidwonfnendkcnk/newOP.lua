7845cf4606839b0ce3856760e988bea70a8cae45
islandmakerepic <mysandwich00@gmail.com>
2015-04-07 12:54:05 -0700

    Create NowYouSeeMe.lua

---
+++ NowYouSeeMe.lua
@@ -0,0 +1,26 @@
+local char=script.Parent
+
+local m=game.Players.LocalPlayer:GetMouse()
+
+
+tp=function(SP,EP)
+
+local mainray=Instance.new("Part",char)
+local dir=CFrame.new(SP.p,EP.p)
+local len=(SP.p-EP.p).magnitude
+local s1=len/2
+mainray.FormFactor='Custom'
+mainray.Size=Vector3.new(1,1,len)
+mainray.CFrame=dir*CFrame.new(0,0,len/2)
+mainray.CanCollide=false
+mainray.Anchored=true
+local ray1=mainray:clone()
+ray1.Size=Vector3.new(0.1,0.1,s1)
+ray1.CFrame=dir*CFrame.Angles(0,0,math.rad(45))*CFrame.new(0,0,len/4)
+ray1.Parent=char
+
+end
+
+m.Button1Down:connect(function()
+tp(char.Torso.CFrame,m.Hit)
+end)
