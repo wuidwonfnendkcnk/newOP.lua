fbc7c0186fd83b4d33eb222b969fdf3662a6869e
islandmakerepic <mysandwich00@gmail.com>
2015-04-07 13:05:51 -0700

    Update NowYouSeeMe.lua

--- NowYouSeeMe.lua
+++ NowYouSeeMe.lua
@@ -8,7 +8,7 @@ tp=function(SP,EP)
 local mainray=Instance.new("Part",char)
 local dir=CFrame.new(SP.p,EP.p)
 local len=(SP.p-EP.p).magnitude
-local s1=len/2
+local s1=len/1.5
 mainray.FormFactor='Custom'
 mainray.Size=Vector3.new(1,1,len)
 mainray.CFrame=dir*CFrame.new(0,0,-(len/2))
@@ -16,11 +16,11 @@ mainray.CanCollide=false
 mainray.Anchored=true
 local ray1=mainray:clone()
 ray1.Size=Vector3.new(0.1,0.1,s1)
-ray1.CFrame=dir*CFrame.Angles(math.rad(15),0,0)*CFrame.new(0,0,-(len/4))
+ray1.CFrame=dir*CFrame.Angles(math.rad(25),0,0)*CFrame.new(0,0,-(len/3))
 ray1.Parent=char
 ray2=ray1:clone()
 ray2.Parent=char
-ray2.CFrame=ray2.CFrame*CFrame.new(0,0,-(len/4))*CFrame.Angles(math.rad(-30),0,0)*CFrame.new(0,0,-(len/4))
+ray2.CFrame=ray2.CFrame*CFrame.new(0,0,-(len/3))*CFrame.Angles(math.rad(-50),0,0)*CFrame.new(0,0,-(len/3))
 end
 
 m.Button1Down:connect(function()
