23558e7dc6aadbf21c842bc3ed4b23a669f708d7
islandmakerepic <mysandwich00@gmail.com>
2015-04-07 12:57:06 -0700

    Update NowYouSeeMe.lua

--- NowYouSeeMe.lua
+++ NowYouSeeMe.lua
@@ -11,12 +11,12 @@ local len=(SP.p-EP.p).magnitude
 local s1=len/2
 mainray.FormFactor='Custom'
 mainray.Size=Vector3.new(1,1,len)
-mainray.CFrame=dir*CFrame.new(0,0,len/2)
+mainray.CFrame=dir*CFrame.new(0,0,-(len/2))
 mainray.CanCollide=false
 mainray.Anchored=true
 local ray1=mainray:clone()
 ray1.Size=Vector3.new(0.1,0.1,s1)
-ray1.CFrame=dir*CFrame.Angles(math.rad(45),0,math.rad(45))*CFrame.new(0,0,len/4)
+ray1.CFrame=dir*CFrame.Angles(math.rad(-45),0,math.rad(-45))*CFrame.new(0,0,-(len/4))
 ray1.Parent=char
 
 end
