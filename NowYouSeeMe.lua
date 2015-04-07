2fd7e3c56279c7b7f099df5e369a01cfd8c09855
islandmakerepic <mysandwich00@gmail.com>
2015-04-07 13:19:58 -0700

    Update NowYouSeeMe.lua

--- NowYouSeeMe.lua
+++ NowYouSeeMe.lua
@@ -8,19 +8,20 @@ tp=function(SP,EP)
 local mainray=Instance.new("Part",char)
 local dir=CFrame.new(SP.p,EP.p)
 local len=(SP.p-EP.p).magnitude
-local s1=len/1.5
+local s1=len/2
 mainray.FormFactor='Custom'
 mainray.Size=Vector3.new(1,1,len)
 mainray.CFrame=dir*CFrame.new(0,0,-(len/2))
 mainray.CanCollide=false
 mainray.Anchored=true
+mainray.Transparency=1
 local ray1=mainray:clone()
 ray1.Size=Vector3.new(0.1,0.1,s1)
-ray1.CFrame=dir*CFrame.Angles(math.rad(25),0,0)*CFrame.new(0,0,-(len/3))
+ray1.CFrame=dir*CFrame.Angles(math.rad(25),0,0)*CFrame.new(0,0,-(len/4))
 ray1.Parent=char
 ray2=ray1:clone()
 ray2.Parent=char
-ray2.CFrame=ray2.CFrame*CFrame.new(0,0,-(len/3))*CFrame.Angles(math.rad(-50),0,0)*CFrame.new(0,0,-(len/3))
+ray2.CFrame=ray2.CFrame*CFrame.new(0,0,-(len/4))*CFrame.Angles(math.rad(-50),0,0)*CFrame.new(0,0,-(len/4))
 local start = ray1.CFrame*CFrame.new(0,0,s1/2)
 local int=5
 for i=1,ray1.Size.Z,int do
@@ -33,9 +34,10 @@ for i=1,ray1.Size.Z,int do
   p.Transparency=0.5
   game.Debris:AddItem(p,0.5)
   start=start*CFrame.new(0,0,-int)
+  char.Torso.CFrame=start
   p.CFrame=start*CFrame.Angles(0,math.rad(math.random(-90,90)),math.rad(math.random(-100,100)))
     p.BrickColor=BrickColor.Random()
-    char.Torso.CFrame=p.CFrame
+
   end
   
   wait(0)
@@ -52,9 +54,10 @@ for i=1,ray2.Size.Z,int do
   p.Transparency=0.5
   game.Debris:AddItem(p,0.5)
   start=start*CFrame.new(0,0,-int)
+  char.Torso.CFrame=start
   p.CFrame=start*CFrame.Angles(0,math.rad(math.random(-90,90)),math.rad(math.random(-100,100)))
     p.BrickColor=BrickColor.Random()
-    char.Torso.CFrame=p.CFrame
+    
   end
 end
 
