1f2588eb46b5871df9bde36e6246b4b09c715b7b
islandmakerepic <mysandwich00@gmail.com>
2015-04-12 20:32:49 -0700

    Update fly.lua

--- fly.lua
+++ fly.lua
@@ -6,9 +6,7 @@ local part=Instance.new("Part",script.Parent)
 part.Anchored=true
 part.Size=Vector3.new(5,1,5)
 local dif=part.CFrame
-local w=Instance.new("Weld",part)
-w.Part0=script.Parent.Torso
-w.Part1=w.Parent
+
 m.Button1Down:connect(function()
 active=active==0 and 1 or 0
 end)
@@ -18,5 +16,5 @@ if active==1 then
 --dif=dif*CFrame.new(0,0,-1)
 end
 part.CFrame = dif
-w.C0=part.CFrame*CFrame.new(0,20,0)*CFrame.Angles(math.rad(90),0,math.rad(90))
+char.Torso.CFrame=part.CFrame*CFrame.new(0,20,0)*CFrame.Angles(math.rad(90),0,math.rad(90))
 end
