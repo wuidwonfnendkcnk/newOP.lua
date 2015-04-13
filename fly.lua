1dd2e0db7c6364e423647ba078241b328e8afb57
islandmakerepic <mysandwich00@gmail.com>
2015-04-12 20:25:07 -0700

    Create fly.lua

---
+++ fly.lua
@@ -0,0 +1,22 @@
+local active = 0
+
+local m=game.Players.LocalPlayer:GetMouse()
+
+local part=Instance.new("Part",owner.Character)
+part.Anchored=true
+part.Size=Vector3.new(5,1,5)
+local dif=part.CFrame
+local w=Instance.new("Weld",part)
+w.Part0=w.Parent
+w.Part1=owner.Character.Torso
+m.Button1Down:connect(function()
+active=active==0 and 1 or 0
+end)
+
+while wait() do
+if active then
+dif=dif*CFrame.new(0,0,-1)
+end
+part.CFrame = dif
+w.C0=part.CFrame*CFrame.new(0,10,0)*CFrame.Angles(0,0,math.rad(90))
+end
