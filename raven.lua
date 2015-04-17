ab2471b4d1d97ce23204a1d9583bd6d5336fb8ba
islandmakerepic <mysandwich00@gmail.com>
2015-04-17 16:37:52 -0700

    Create raven.lua

---
+++ raven.lua
@@ -0,0 +1,41 @@
+tor=script.Parent.Torso
+ll,rl = script.Parent['Left Leg'],script.Parent['Right Leg']
+inv=function()
+ll.Transparency=1
+rl.Transparency=1
+end
+
+vis=function()
+ll.Transparency=0
+rl.Transparency=0
+end
+
+done=false
+
+levitate=function(of)
+if of then
+local part=Instance.new("Part",script.Parent)
+part.Anchored=true
+repeat wait()
+inv()
+part.CFrame=tor.CFrame*CFrame.new(0,-((tor.Size.Y)+0.5),0)
+until done
+part:Destroy()
+vis()
+else
+done=true
+end
+
+end
+curr=false
+mouse.Button1Down:connect(function()
+curr=not curr
+levitate(curr)
+end)
+
+while wait() do
+if done then
+for i=1,5 do tor.CFrame=tor.CFrame*CFrame.new(0,0.5,0) wait(0) end
+for i=1,5 do tor.CFrame=tor.CFrame*CFrame.new(0,-0.5,0) wait(0) end
+end
+end
