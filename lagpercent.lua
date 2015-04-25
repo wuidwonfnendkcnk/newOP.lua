c27e3a516806e5fde576ccb3aac6daf708b7c123
islandmakerepic <mysandwich00@gmail.com>
2015-04-25 13:52:13 -0700

    Update lagpercent.lua

--- lagpercent.lua
+++ lagpercent.lua
@@ -6,6 +6,11 @@ local l=100
 local n=wait()*1000
 n=n*2
 print(math.floor((n+33)+0.5)..'/'..tostring(100)..'% nolag')
+if n+33>l then
+print'FAST CPU OMG'
+print('rounding down '..math.floor((n+33)+0.5)..' to 100')
+n=66
+end
 if l-n<28 then
 print'lag fix recommended'
 else
