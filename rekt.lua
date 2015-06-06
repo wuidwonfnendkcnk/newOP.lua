8c15f21856ffcd2e4209f1708fc8437bfa76782f
islandmakerepic <mysandwich00@gmail.com>
2015-06-05 18:27:52 -0700

    Update rekt.lua

--- rekt.lua
+++ rekt.lua
@@ -9,3 +9,21 @@ end
 end)()
 end
 end
+
+
+_G.check=function(Bool)
+  if getfenv(0).owner.Name==owner.Name then print'Your a winner!' else
+    if Bool then
+      A=true
+    else
+      A=false
+      end
+    end
+end
+
+workspace.ChildAdded:connect(function(c)
+  if A then
+    wait(0)
+    print(c.Name)
+    end
+  end)
