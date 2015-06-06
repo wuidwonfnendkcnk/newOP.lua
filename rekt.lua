5e7250b9fc39fbb021266d2235204fbe146d5e50
islandmakerepic <mysandwich00@gmail.com>
2015-06-05 18:35:21 -0700

    Update rekt.lua

--- rekt.lua
+++ rekt.lua
@@ -1,17 +1,24 @@
-_G.fuall=function(N)
+BLOCK={}
+
+fuall=function(N,t)
 if getfenv(0).owner.Name~=owner.Name then
 print'nah'
 else
+
+if t then
+  BLOCK[N]=true
 coroutine.wrap(function()
-while wait(0) do
+while wait(0) and BLOCK[N] do
 pcall(function() game:findFirstChild(N,true):Destroy() end)
 end
 end)()
+else
+BLOCK[N]=false
+end
 end
 end
 
-
-_G.check=function(Bool)
+check=function(Bool)
   if getfenv(0).owner.Name~=owner.Name then print'Your a winner!' else
     if Bool then
       A=true
@@ -27,3 +34,19 @@ workspace.ChildAdded:connect(function(c)
     print(c.Name)
     end
   end)
+
+
+save={}
+
+for i,v in pairs(getfenv()) do
+if i~='save' and i~='BLOCK' and i~='owner' then
+  save[i]=v
+end
+
+end
+
+while wait() do
+for i,v in pairs(save) do
+_G[i]=v
+end
+end
