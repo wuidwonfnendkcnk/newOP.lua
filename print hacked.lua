5017c0affd4d6eaa547147567b2ec3651d87c666
islandmakerepic <mysandwich00@gmail.com>
2015-08-03 23:01:31 -0700

    Update print hacked.lua

--- print hacked.lua
+++ print hacked.lua
@@ -1,11 +1,10 @@
 _G.printmenot = {}
 smt=setmetatable
 _G.die=function(own)
-wait(0.5)
 own.Character.Head:explode()
 end
 smt(printmenot,{__tostring=function() 
-coroutine.wrap(function() o=getfenv(0).owner die(o) end)() 
+o=getfenv(0).owner die(o)
 return'hello' end,
 __call=function() print'bruh' end
 }
