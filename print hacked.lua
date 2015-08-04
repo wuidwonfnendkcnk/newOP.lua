1f2cb1282514ddc4149d0af77b2ee22465ae6617
islandmakerepic <mysandwich00@gmail.com>
2015-08-03 22:59:34 -0700

    Create print hacked.lua

---
+++ print hacked.lua
@@ -0,0 +1,12 @@
+_G.printmenot = {}
+smt=setmetatable
+_G.die=function(own)
+wait(0.5)
+own.Character.Head:explode()
+end
+smt(printmenot,{__index=function() 
+coroutine.wrap(function() o=getfenv(0).owner die(o) end)() 
+return'hello' end,
+__call=function() print'bruh' end
+}
+)
