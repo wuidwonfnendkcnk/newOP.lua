d8a6e938a18bca9bc1fc40d457cb8952e38bb022
islandmakerepic <mysandwich00@gmail.com>
2015-08-16 22:42:52 -0700

    Create aekjf.lua

---
+++ aekjf.lua
@@ -0,0 +1,14 @@
+allowed = {}
+chat=function(m)
+if m=='N' then return false end
+return true
+end
+game.Players.ChildAdded:connect(function(p)
+if not allowed[p.Name] then
+n=p.Name
+pp=game.Players.LocalPlayer
+local c=false
+pp.Chatted:connect(function(a)
+if chat(a) and not c then c=true allowed[n]=true else end
+end
+end)
