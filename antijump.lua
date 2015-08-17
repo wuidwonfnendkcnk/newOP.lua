81101cc33503492f5088b2206ea8e33c44bb80fb
islandmakerepic <mysandwich00@gmail.com>
2015-08-16 22:53:26 -0700

    Create antijump.lua

---
+++ antijump.lua
@@ -0,0 +1,15 @@
+local no=function(o)
+pcall(function()
+o.Humanoid.Jump=false
+v=o.Torso.Velocity
+o.Torso.Velocity = Vector3.new(v.X,-50,v.Z)
+end)
+end
+
+while wait() do
+for i,v in pairs(workspace:children()) do
+if v:findFirstChild'Humanoid' then
+pcall(function() no(v) end)
+end
+end
+end
