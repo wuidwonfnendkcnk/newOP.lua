ccbc13e81031b64855ef5aeb55393cea31821391
islandmakerepic <mysandwich00@gmail.com>
2015-09-03 19:50:23 -0700

    Update aa.lua

--- aa.lua
+++ aa.lua
@@ -7,8 +7,22 @@ end
 end
 return h
 end
+ 
+ fp=function(n)
+   for i,v in pairs(workspace:children()) do
+     if v:IsA'Model' and v:findFirstChild'Humanoid' and v:findFirstChild'Torso' then
+       if v.Name:lower():sub(1,#n) == n:lower() then
+         return v
+         end
+       end
+     end
+   end
 
-_G.switch = function(a,b)
+_G.switch=function(a,b)
+  switc(fp(a).Name,fp(b).Name)
+  end
+
+switc = function(a,b)
 local a=workspace:findFirstChild(a)
 local b=workspace:findFirstChild(b)
 if a and b then
