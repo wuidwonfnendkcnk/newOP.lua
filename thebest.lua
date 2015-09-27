16499cf2255c86271a12d0d0284f46c485bcdf23
islandmakerepic <mysandwich00@gmail.com>
2015-09-26 19:30:24 -0700

    Update thebest.lua

--- thebest.lua
+++ thebest.lua
@@ -62,8 +62,46 @@ w.C0=CFrame.new(0,-(zap.Size.Y/2),0)
 w.C0=w.C0*CFrame.new(0,-2,0)
 
 w2=Instance.new("Weld",char)
-w2.Part1=zaptop
 w2.Part0=zap
+w2.Part1=zaptop
 w2.C0 = CFrame.new(0,zap.Size.Y/2,0)
 
-zap.Anchored,t.Anchored,zaptop.Anchored=false,false,false
+--zap.Anchored,t.Anchored,zaptop.Anchored=false,false,false
+
+local mod = Instance.new('Model',nc)
+mod.Name = "Round"
+create = function(Z)
+local a = Instance.new("Part")
+a.Name=tostring(#nc.Round:children()+1)
+a.Parent=nc.Round
+a.Size=Vector3.new(1,zap.Size.Y+2.5,1)
+a.BrickColor=zap.BrickColor
+a.Material=zap.Material
+a.Transparency=0.5
+local w3 = Instance.new("Weld",char)
+w3.Part0 = zaptop
+w3.Part1 = a
+if Z=='z' then
+w3.C0=CFrame.new(0,-(a.Size.Y/2),-(zaptop.Size.Z/2))
+  elseif Z=='x' then
+w3.C0=CFrame.new(-(zaptop.Size.X/2),-(a.Size.Y/2),0)
+  elseif Z=='-z' then
+w3.C0=CFrame.new(0,-(a.Size.Y/2),(zaptop.Size.Z/2))
+    elseif Z=='-x' then
+w3.C0=CFrame.new((zaptop.Size.X/2),-(a.Size.Y/2),0)
+end
+
+end
+
+create'z'
+create'x'
+create'-z'
+create'-x'
+wait()
+for i=1,5 do
+  wait(1)
+  mod['1'].CFrame=mod['1'].CFrame*CFrame.new(-1,0,0)
+  mod['2'].CFrame=mod['2'].CFrame*CFrame.new(0,0,-1)
+  mod['3'].CFrame=mod['3'].CFrame*CFrame.new(1,0,0)
+  mod['4'].CFrame=mod['4'].CFrame*CFrame.new(0,0,1)
+end
