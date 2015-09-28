b12834074504ee6e25891dae7e25cdcdd9c135da
islandmakerepic <mysandwich00@gmail.com>
2015-09-27 22:37:31 -0700

    Update thebest.lua

--- thebest.lua
+++ thebest.lua
@@ -338,7 +338,7 @@ Sine=Sine+Change
 -- STUFF TO DETECT MOVING, ETC
 if Moving then
   
-WELD22.C0=L(WELD22.C0,POS*CFrame.Angles(math.sin(Sine/3)*0.3,0,0),0.1) -- CFRAMETOLERP:Lerp(POS,SPEED)
+WELD22.C0=L(WELD22.C0,POS*CFrame.Angles(math.sin(Sine/3)*0.3,0,0),0.2) -- CFRAMETOLERP:Lerp(POS,SPEED)
 WELD2.C0= L(WELD2.C0,POS2*CFrame.Angles(math.sin(Sine/3)*0.3,0,0),0.1) -- CFRAMETOLERP:Lerp(POS,SPEED)
 
 end
