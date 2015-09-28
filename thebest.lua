8e27d987d1e1b11c5ac4d7503b32d0f579317067
islandmakerepic <mysandwich00@gmail.com>
2015-09-27 21:58:27 -0700

    Update thebest.lua

--- thebest.lua
+++ thebest.lua
@@ -338,6 +338,6 @@ Sine=Sine+Change
 if Moving then
 WELD22.C0=L(WELD22.C0,POS*CFrame.Angles(math.sin(Sine/3)*0.3,0,0),0.3) -- CFRAMETOLERP:Lerp(POS,SPEED)
 WELD2.C0=L(WELD2.C0,POS2*CFrame.Angles(math.sin(Sine/3)*0.3,0,0),0.3) -- CFRAMETOLERP:Lerp(POS,SPEED)
-- Experiment with the angles stuff ;p
+
 end
 end
