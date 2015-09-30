1aeec7def6158d3640a2e211ec45e571e3bfb00d
islandmakerepic <mysandwich00@gmail.com>
2015-09-29 22:35:08 -0700

    Update thebest.lua

--- thebest.lua
+++ thebest.lua
@@ -335,7 +335,7 @@ end
 wait(3)
 For(weldzL,-3,150)
 for i,v in pairs(weldzL) do
-  v.C0=v.C0*CFrame.new(5,0,0)
+  v.C0=v.C0*CFrame.new(50,0,0)
   end
 local Handle= Instance.new("Part")
 while true do
