3a2440a7fc2d91b5861a348326b6c7c45656b2d4
islandmakerepic <mysandwich00@gmail.com>
2015-09-30 16:57:36 -0700

    Update thebest2.lua

--- thebest2.lua
+++ thebest2.lua
@@ -187,7 +187,7 @@ for i,v in pairs(weldz) do
 
 
 local arm1 = Instance.new("Model",nc)
-arm1.Name='Left Arm'
+arm1.Name='Right Arm'
 local X,Z=bottom.Size.X,bottom.Size.Z
 YY=(X+Z)/4
 local limb = Instance.new("Part",arm1)
@@ -210,6 +210,32 @@ aWeld2.Part0=limb
 aWeld2.Part1=limb2
 aWeld2.C0 = CFrame.new(0,0,0)
 aWeld2.C0=aWeld2.C0*CFrame.new(20,-(YY*7.5),0)*CFrame.Angles(0,0,math.rad(50))*CFrame.new(0,-10,0)
+
+local arm2 = Instance.new("Model",nc)
+arm2.Name='Right Arm'
+local X,Z=bottom.Size.X,bottom.Size.Z
+YY=(X+Z)/4
+local 2limb = Instance.new("Part",arm2)
+2limb.Name='Limb1'
+2limb.Size = Vector3.new(X,YY*10,Z)
+local a2Weld1 = Instance.new("Weld",bottom)
+a2Weld1.Part0=bottom
+a2Weld1.Part1=2limb
+a2Weld1.C0 = CFrame.new(0,0,0)
+a2Weld1.C0=a2Weld1.C0*CFrame.Angles(0,0,math.rad(-30))*CFrame.new(0,-(YY*5),0)
+limb.BrickColor=BrickColor.New'Brown'
+limb.Material='Grass'
+local 2limb2 = 2limb:clone()
+2limb2.Parent=arm1
+2limb2:breakJoints()
+2limb2.Name='Limb2'
+2limb2.Size=Vector3.new(X,YY*15,Z)
+local a2Weld2 = Instance.new("Weld",limb)
+a2Weld2.Part0=2limb
+a2Weld2.Part1=2limb2
+a2Weld2.C0 = CFrame.new(0,0,0)
+a2Weld2.C0=a2Weld2.C0*CFrame.new(20,-(YY*7.5),0)*CFrame.Angles(0,0,math.rad(-50))*CFrame.new(0,-10,0)
+
 
 wait(10)
 
