7084e811deea11cc587524fe0e4b92cd89dbc509
islandmakerepic <mysandwich00@gmail.com>
2015-09-29 23:07:19 -0700

    Update thebest.lua

--- thebest.lua
+++ thebest.lua
@@ -264,6 +264,7 @@ local For=function(T,ang,orig,ok,ok2,half)
         if half and i>=#T/2 then
           --print(v.C1)
         v.C1=v.C0*CFrame.Angles(0,0,math.rad(0,0,math.rad(90)))
+          v.C0=v.C0*CFrame.new(i*5,0,0)
           --print(v.C1)
           --print'rot extra'
         end
