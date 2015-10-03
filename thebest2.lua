bd7a3e79b81228a77f47aa4d3f20082ba7cca34a
islandmakerepic <mysandwich00@gmail.com>
2015-10-02 19:03:06 -0700

    Update thebest2.lua

--- thebest2.lua
+++ thebest2.lua
@@ -312,7 +312,7 @@ end
 wait(3)
 
     aWeld2.C0=CFrame.new(0,10,0)
-    aWeld2.C0=aWeld2.C0*CFrame.new(0,-(YY*7.5),0)*CFrame.Angles(0,0,math.rad(95))*CFrame.new(0,-35,0)
+    aWeld2.C0=aWeld2.C0*CFrame.new(0,-(YY*7.5),0)*CFrame.Angles(0,0,math.rad(130))*CFrame.new(0,-35,0)
   
 bottom.Anchored=false
 local Handle= Instance.new("Part")
