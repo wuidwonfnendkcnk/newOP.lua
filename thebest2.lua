da0bde54c622d09048ca0a42cc4410f7679cf440
islandmakerepic <mysandwich00@gmail.com>
2015-10-03 14:37:29 -0700

    Update thebest2.lua

--- thebest2.lua
+++ thebest2.lua
@@ -347,10 +347,10 @@ StHWeld.Part0=Handle
 StHWeld.Part1=STop
 StHWeld.C0=CFrame.new(0,-(Handle.Size.Y/2),0)*CFrame.new(0,-20,0)
 
-local w1=Instance.new("WedgePart",Sword)
+local w1=Instance.new("Part",Sword)
 w1.FormFactor='Custom'
 w1.Size=Vector3.new(10,20,10)
-
+Instance.new("SpecialMesh",w1).MeshType='Wedge'
 local w1Weld=Instance.new("Weld",STop)
 
 w1Weld.Part0=STop
