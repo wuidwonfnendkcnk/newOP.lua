28b8782c94315129c750419076fa59e31533a6ae
islandmakerepic <mysandwich00@gmail.com>
2015-10-09 19:36:23 -0700

    Update thebest2.lua

--- thebest2.lua
+++ thebest2.lua
@@ -358,6 +358,7 @@ Fire=function(pos)
   local rek=false
   Ball.Touched:connect(function(h)
     pcall(function() if h.Parent~=p.Character then h.Parent.Humanoid:TakeDamage(20) end end)
+    Instance.new("Explosion",Ball).BlastRadius=100
     Ball.Anchored=true
     if rek then return end
     rek=true
