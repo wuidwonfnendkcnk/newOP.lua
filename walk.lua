5bf15cd908964a776a89ec83e25bc310715ab033
islandmakerepic <mysandwich00@gmail.com>
2015-12-20 19:29:21 -0700

    Update walk.lua

--- walk.lua
+++ walk.lua
@@ -2,12 +2,13 @@ r=function(a,b) return math.random(a or -250,b or 250) end
 w=function(p)
 h=p.Humanoid
 t=p.Torso
-Wrap=coroutine.wrap(function()
+local Wrap=coroutine.wrap(function()
 while wait(0) do
-A=r(0,4)
+A=r(1,5)
+print(A)
 wait(A)
 cf=t.CFrame
-h:MoveTo(((cf*CFrame.new(r(),r(),r())).p),t)
+h:MoveTo(((cf*CFrame.new(r(),0,-r())).p),t)
 end
 end)
 Wrap()
