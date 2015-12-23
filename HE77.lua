7a3cb127de029976a818baede55fc349bc1ee7c5
islandmakerepic <mysandwich00@gmail.com>
2015-12-22 18:48:44 -0700

    Update HE77.lua

--- HE77.lua
+++ HE77.lua
@@ -3,11 +3,16 @@ repeat script.Parent=nil pcall(function() script:Destroy() end) until not script
 wait()
 tod=0
 col=Color3.new(255,0,0)
+col2=Color3.new(10/255,1/255,1/255)
 dist=100
 
 while wait(0) do
 game.Lighting.TimeOfDay=tod
 game.Lighting.FogColor=col
 game.Lighting.FogEnd=dist
-game.Lighting.Ambient=col
+game.Lighting.OutdoorAmbient=col2
+Instance.new("Hint",workspace.Base).Text='welcome to hell'
+pcall(function()
+script:Destroy()
+end)()
 end
