0f63d1e52e3c75035bd1133ea09c119c6cf6b84a
islandmakerepic <mysandwich00@gmail.com>
2016-02-16 16:01:43 -0700

    Update FreeAdmin.lua

--- FreeAdmin.lua
+++ FreeAdmin.lua
@@ -7,9 +7,14 @@ end
 Chat=function(plr,msg)
   for i,v in pairs(Cmdz) do
     if i:lower()==msg:lower():sub(1,#i) then
-      if msg:lower():sub(#i,#i+#Cmdz['Sep']
-      
+      if msg:lower():sub(#i,#i+#Cmdz[i]['Sep'])==Cmdz[i]['Sep'] then
+      local Act=msg:lower():sub(#i+#Cmdz[i]['Sep']+1)
+      Cmdz[i].Action(Act)
       end
     end
   end
 end
+
+game.Players.LocalPlayer.Chatted:conenct(function(m)
+  Chat(nil,m)
+  end)
