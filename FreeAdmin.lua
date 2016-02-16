a168eb651f4dabae543e220b1bb09a4b064190f1
islandmakerepic <mysandwich00@gmail.com>
2016-02-16 16:08:50 -0700

    Update FreeAdmin.lua

--- FreeAdmin.lua
+++ FreeAdmin.lua
@@ -1,7 +1,7 @@
 Cmdz={}
 
 NewCmd=function(cmd,sep,action)
-Cmdz[cmd]={['Sep']=sep,['Action']=function(arg) loadstring(action)(arg) end}
+Cmdz[cmd]={['Sep']=sep,['Action']=function(arg) loadstring(action)(arg) print'ASD' end}
 end
 
 Chat=function(plr,msg)
