742f14e437db90eda2593b1305fc7e2af6f45269
islandmakerepic <mysandwich00@gmail.com>
2016-02-19 18:39:17 -0700

    Update fiel.lua

--- fiel.lua
+++ fiel.lua
@@ -20,17 +20,15 @@ cln.Anchored=true
 cln.Size=Vector3.new(1,1,3)
 local curr=1
 while wait(0) do
-	local spot=owner.Character.Torso.CFrame
+local spot=owner.Character.Torso.CFrame
 ang=ang+5
 curr=curr+1
 if curr>10 then curr=0 end
 block.CFrame=spot*CFrame.Angles(math.rad(-ang),math.rad(ang),math.rad(ang/10))*CFrame.new(-50,0,0)
 local cln=cln:Clone()
-
 cln.Parent=workspace
-cln.CFrame=script.Parent.CFrame*CFrame.new(0,0,-1)
-cln.Transparency=tonumber('0.'..curr)
+cln.CFrame=block.CFrame*CFrame.new(0,0,-1)
+cln.Transparency=(curr/10)
 cln.BrickColor=BrickColor.new('Toothpaste')
 game.Debris:AddItem(cln,1)
-
 end
