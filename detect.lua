1da3df1119f86bf30409c3f30b75b21f5bb23699
islandmakerepic <mysandwich00@gmail.com>
2016-02-28 15:37:40 -0700

    Update detect.lua

--- detect.lua
+++ detect.lua
@@ -219,7 +219,7 @@ for i=1,3 do sphere.Size=sphere.Size+Vector3.new(1,0,0) wait() end
 local CAN=true
 sphere.Touched:connect(function(h) 
   if not CAN then return end
-  if h.Parent~=char then Endit(h,true) if h.Name=='Torso' then link(part,h,12) end
+  if h.Parent~=char then Endit(h,true) if h.Name=='Torso' then link(part,h,12) end end
 end)
 CFZ=sphere.CFrame
 asd=5
