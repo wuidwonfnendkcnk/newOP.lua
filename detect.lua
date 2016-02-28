cde8448733f7b68896fee5abe02650ef7f38b805
islandmakerepic <mysandwich00@gmail.com>
2016-02-28 16:55:28 -0700

    Update detect.lua

--- detect.lua
+++ detect.lua
@@ -282,7 +282,7 @@ bl.CFrame=start*CFrame.new(0,0,-3)
 for i=1, 5, 0.5 do
   bl.Material='Grass'
   bl.Size=bl.Size+Vector3.new(1,1,1)
-  bl.CFrame=start*CFrame.new(0,0,-1)
+  bl.CFrame=start*CFrame.new(0,0,-i)
   wait(0)
   end
 end
