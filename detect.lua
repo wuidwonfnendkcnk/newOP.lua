f3b17463174c1519c98cfb2a17c86f28267c2434
islandmakerepic <mysandwich00@gmail.com>
2016-02-28 17:16:17 -0700

    Update detect.lua

--- detect.lua
+++ detect.lua
@@ -323,7 +323,7 @@ m.KeyDown:connect(function(k)
     m.Move:connect(function()
       if not connected then return end
       if connected then
-       CLerp(Control.Part,CFrame.new(Main.p,m.Hit.p),0.2)
+       CLerp(Control.Part,CFrame.new(Main.p,m.Hit.p),0.09)
         end
       end)
   end
