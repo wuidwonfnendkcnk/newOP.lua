4ecb2b6dc2121efc9402dfe6fd8e9b772f1a0413
islandmakerepic <mysandwich00@gmail.com>
2016-02-28 18:11:22 -0700

    Update detect.lua

--- detect.lua
+++ detect.lua
@@ -351,6 +351,15 @@ m.KeyDown:connect(function(k)
      
       if connected then
        CLerp(Control.Part,CFrame.new(Main.p,m.Hit.p),0.2)
+       local p=Instance.new("Part",workspace)
+       game.Debris:AddItem(p,0)
+       p.Size=Vector3.new(3,3,3)
+       p.Transparency=0.5
+       p.BrickColor=BrickColor.New'Lime green'
+       p.Anchored=true
+       p.CanCollide=false
+       p.CFrame=Control.Part.CFrame*CFrame.new(0,0,-120)
+       p.CFrame=CFrame.new(p.CFrame.X,1,p.CFrame.Z)
         end
       until not connected
   end
