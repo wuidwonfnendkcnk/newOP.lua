8c56c5915d782b7dc345627a56131cca6c70ec94
islandmakerepic <mysandwich00@gmail.com>
2016-03-01 11:29:33 -0700

    Update block.lua

--- block.lua
+++ block.lua
@@ -66,7 +66,7 @@ while wait() do
       for i=1,block.JumpPower.Value do
         print''
         wait(0)
-      block.Velocity=block.Velocity+Vector3.new(0,block.JumpPower.Value,0)
+      block.Velocity=block.Velocity+Vector3.new(0,block.JumpPower.Value*4,0)
     end
     for i=1,block.JumpPower.Value do
       print'asd'
