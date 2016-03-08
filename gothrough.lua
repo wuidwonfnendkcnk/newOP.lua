50ed437aba93bf8125632eb08b8d2bbf692d5f4e
islandmakerepic <mysandwich00@gmail.com>
2016-03-08 02:02:14 -0700

    Update gothrough.lua

--- gothrough.lua
+++ gothrough.lua
@@ -19,7 +19,17 @@ block.Size=Vector3.new(detectRange/2,detectRange/4,detectRange)
 block.CanCollide=false
 block.Anchored=false
 block.Touched:connect(function(h)
-  if not (h.Parent==workspace or h.Parent.Parent==workspace or not h.Parent:IsA'Tool' or h.Parent.Name~=block.Name) then return end
+  if h.Parent==workspace then
+    if h.Name=='Base' or h.Name==block.Name then return end
+  else
+    if h.Parent.Parent==workspace then
+      if h.Parent==script.Parent then return end
+      if h.Parent:IsA'Hat' or h.Parent:IsA'Tool' then return end
+      
+    else
+      return
+      end
+    end
 local size=(h.Size.X+h.Size.Y+h.Size.Z)/3
 size=size+detectRange+3
 script.Parent.Torso.CFrame=script.Parent.Torso.CFrame*CFrame.new(0,0,-size)
@@ -28,6 +38,7 @@ for i=1,detectRange,2 do
 wait(0)
 block.CFrame=script.Parent.Torso.CFrame*CFrame.new(0,0,-i)*CFrame.new(0,0,-block.Size.Z/2)*CFrame.new(0,0,-1)
 end
+game.Debris:AddItem(block,0.001)
 end
 end)
 repeater()
