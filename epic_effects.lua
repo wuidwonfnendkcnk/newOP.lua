302a22ee22596081d0d39dd63a7dfaec7b792e00
islandmakerepic <mysandwich00@gmail.com>
2016-03-09 10:23:00 -0700

    Update epic_effects.lua

--- epic_effects.lua
+++ epic_effects.lua
@@ -12,7 +12,7 @@ end
 
 p=game.Players.LocalPlayer
 c=p.Character
-for asd=1,32,4 do
+for asd=0,50,10 do
 wait(0)
   for i=0,360,20 do
      if i==100 or i==200 or i==300 then
@@ -20,6 +20,6 @@ wait(0)
         wait(0)
         end
       end
-sp(Vector3.new(2,2,2),c.Torso.CFrame*CFrame.Angles(0,math.rad(i),0)*CFrame.Angles(0,0,math.rad(45))*CFrame.new(0,asd,-20),1.5)
+sp(Vector3.new(4,4,4),c.Torso.CFrame*CFrame.Angles(0,math.rad(i),0)*CFrame.Angles(0,0,math.rad(45))*CFrame.new(0,asd,-20),1.5)
   end
 end
