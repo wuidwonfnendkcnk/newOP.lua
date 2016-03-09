c237913bf7f0fe5dd659223276b370be1e338452
islandmakerepic <mysandwich00@gmail.com>
2016-03-09 11:07:19 -0700

    Update epic_effects.lua

--- epic_effects.lua
+++ epic_effects.lua
@@ -52,7 +52,7 @@ sp(Vector3.new(10,10,10),c.Torso.CFrame*CFrame.Angles(0,math.rad(i),0)*CFrame.An
   end
 end
 end)()
-for i=1,50,2 do wait(0) 
+for i=1,50,5 do wait(0) 
 cff=block.CFrame block.Size=block.Size+Vector3.new(2,0,2) block.CFrame=cff 
 block.Transparency=i/50 
 end
