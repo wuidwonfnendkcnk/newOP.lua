7de32141f7161c9b30ed868e4813363333e36fbe
islandmakerepic <mysandwich00@gmail.com>
2016-03-10 03:06:40 -0700

    Update epic_effects.lua

--- epic_effects.lua
+++ epic_effects.lua
@@ -325,7 +325,8 @@ m.KeyDown:connect(function(k)
     	mr=math.random
     	--*CFrame.Angles(r(mr(100)),r(mr(100)),r(mr(100)))
     	local spot=block.CFrame*CFrame.new(0,100,-20)
-    	coroutine.wrap(function() wait(0.5) while wait(0) do CLerp(block,spot,0.05) end end)()
+    	block.CanCollide=false
+    	coroutine.wrap(function() wait(0.5) while wait(0) and block and block.Parent do CLerp(block,spot,0.05) end end)()
       for i=1,10,0.35 do 
       	wait(0) block.Transparency=i/10 end
       block:Destroy()
