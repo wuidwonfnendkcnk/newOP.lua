7156b6576ade342dda4016a248d830d40e90ce48
islandmakerepic <mysandwich00@gmail.com>
2016-03-12 14:30:31 -0700

    Update moleBeta.lua

--- moleBeta.lua
+++ moleBeta.lua
@@ -126,7 +126,7 @@ end
  end
 elseif digging and odear then
  odear=false
- repeat np.CFrame=np.CFrame*CFrame.new(0,1,0)
+ repeat np.CFrame=np.CFrame*CFrame.new(0,1,-0.25)
   Attachblock.CFrame=CFrame.new(Attachblock.Position,np.CFrame.p)*CFrame.new(0,0,-1)
   char.Torso.CFrame=Attachblock.CFrame*CFrame.Angles(math.rad(-90),0,0)
   wait(0) until Attachblock.CFrame.Y>4
