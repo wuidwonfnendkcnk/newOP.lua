4b2dc5a4ccd832bd4c8fa1808d32a78574c83f2a
islandmakerepic <mysandwich00@gmail.com>
2016-03-12 13:30:27 -0700

    Update moleBeta.lua

--- moleBeta.lua
+++ moleBeta.lua
@@ -91,12 +91,13 @@ if odear then
  Attachblock.CFrame=Attachblock.CFrame*CFrame.Angles(0,0,dir or 0)
 end
 if asd~=0 then
+ if ang==0 then curr=Attachblock.CFrame end
 ang=ang+5
 if ang<45 then
  Attachblock.CFrame=Attachblock.CFrame*CFrame.Angles(math.rad(5),0,0)*CFrame.new(0,(asd),0)
 elseif ang<135 then
  Attachblock.CFrame=Attachblock.CFrame*CFrame.Angles(math.rad(-5),0,0)*CFrame.new(0,(asd),0)
- else ang=0
+ else ang=0 Attachblock.CFrame=curr*CFrame.new(0,27,0)
 end
 end
 
