aa40e7699c5e7d151684997077eda8b42b331beb
islandmakerepic <mysandwich00@gmail.com>
2016-03-12 12:34:02 -0700

    Update moleBeta.lua

--- moleBeta.lua
+++ moleBeta.lua
@@ -16,11 +16,9 @@ end)
 while wait() do
  if mole then
     if not digging then
-      local hole=Instance.new("Part",workspace.Base)
+      
       digging=true
-      hole.Size=Vector3.new(3,1,3)
-      hole.BrickColor=BrickColor.Black()
-      hole.Anchored=true
+      
       char.Humanoid.PlatformStand=true
       hole.CFrame=CFrame.new(char.Torso.CFrame.X,0.5,char.Torso.CFrame.Z)
       char.Torso.Anchored=true
@@ -30,14 +28,20 @@ while wait() do
         for i=0,-135,-10 do
     cf2=cf*CFrame.Angles(math.rad(i),0,0)*CFrame.new(0,0,-i/30)
   end
-for i=1,5 do
+for i=1,10 do
 cf2=cf2*CFrame.new(0,1,0)
 end
+
   for i=0,-135,-10 do
     Attachblock.CFrame=cf*CFrame.Angles(math.rad(i),0,0)*CFrame.new(0,0,-i/30)
     char.Torso.CFrame=Attachblock.CFrame
     wait(0)
   end
+  local hole=Instance.new("Part",workspace.Base)
+  Instance.new("CylinderMesh",hole)
+  hole.Size=Vector3.new(3,1,3)
+      hole.BrickColor=BrickColor.Black()
+      hole.Anchored=true
   hole.CFrame=CFrame.new(cf2.X,0.5,cf2.Z)
 for i=1,20 do
 Attachblock.CFrame=Attachblock.CFrame*CFrame.new(0,1,0)
