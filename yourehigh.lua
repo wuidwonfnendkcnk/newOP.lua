3b75a913d77e9e7b015661c8d218ad24bb4b3cfd
islandmakerepic <mysandwich00@gmail.com>
2016-03-15 02:47:45 -0700

    Update yourehigh.lua

--- yourehigh.lua
+++ yourehigh.lua
@@ -8,19 +8,17 @@ end
 
 while wait(0) do
 repeat wait(0)
-if owner.Character then
+            if owner.Character then
+                tt=owner.Character:findFirstChild'Torso'
+                    if tt then
+                        last=tt.CFrame or last
+                    end
+            end
 tt=owner.Character:findFirstChild'Torso'
-if tt then
-last=tt.CFrame or last
-end
-endracter then
-tt=owner.Character:findFirstChild'Torso'
-if tt then
-last=tt.CFrame or last
-end
-end
+    if tt then
+        last=tt.CFrame or last
+    end
 wait(0)
 until not owner.Character.Humanoid.Health<1 or not owner.Character or not tt 
 resp()
 end
-
