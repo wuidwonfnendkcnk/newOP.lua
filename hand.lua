d28f04c905f55eff0243d3d52936fbcaed987e30
islandmakerepic <mysandwich00@gmail.com>
2016-03-16 02:04:38 -0700

    Update hand.lua

--- hand.lua
+++ hand.lua
@@ -26,6 +26,25 @@ can=true
 mouse.Button1Down:connect(function() if not can then return end active=true end)
 
 mouse.Button1Up:connect(function() active=false end)
+
+
+local CLerp=function(p,cf2,v)
+
+        local st,cf1=tick'',p.CFrame;local c1,c2,c3,c4,c5,c6,c7,c8,c9,c10,c11,c12=cf1:components'';
+
+        local s1,s2,s3,s4,s5,s6,s7,s8,s9,s10,s11,s12=cf2:components'';
+
+        local glv=function(v1,v2)return v1+(v2-v1)*v*(1+tick''-st);end;
+
+        p.CFrame=CFrame.new(glv(c1,s1),glv(c2,s2),glv(c3,s3),glv(c4,s4),glv(c5,s5)
+
+        ,glv(c6,s6),glv(c7,s7),glv(c8,s8),glv(c9,s9),glv(c10,s10),glv(c11,s11),glv(c12,s12));wait'';
+
+end
+
+
+
+
 num=0
 maxi=75
 local asd=Instance.new("Part",workspace.Terrain)
@@ -35,7 +54,7 @@ asd.Transparency=1
 asd.CanCollide=false
 asd.Anchored=true
 while wait(0) do
-  CLerp(asd,m.Hit,0.075)
+  CLerp(asd,mouse.Hit,0.075)
   if active==true then
     local part=create()
     num=num+1
