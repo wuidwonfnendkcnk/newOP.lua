1ba1fe4f51eccf4a28443f8e22110d721ab26aec
islandmakerepic <mysandwich00@gmail.com>
2016-03-16 04:17:34 -0700

    Update bill.lua

--- bill.lua
+++ bill.lua
@@ -26,6 +26,20 @@ part2=part1:clone()
 part2.Parent=hat
 part2.Size=part2.Size+Vector3.new(0,2,0)
 part1.Anchored,part2.Anchored=true,true
+local CLerp=function(p,cf2,v)
+
+        local st,cf1=tick'',p.CFrame;local c1,c2,c3,c4,c5,c6,c7,c8,c9,c10,c11,c12=cf1:components'';
+
+        local s1,s2,s3,s4,s5,s6,s7,s8,s9,s10,s11,s12=cf2:components'';
+
+        local glv=function(v1,v2)return v1+(v2-v1)*v*(1+tick''-st);end;
+
+        p.CFrame=CFrame.new(glv(c1,s1),glv(c2,s2),glv(c3,s3),glv(c4,s4),glv(c5,s5)
+
+        ,glv(c6,s6),glv(c7,s7),glv(c8,s8),glv(c9,s9),glv(c10,s10),glv(c11,s11),glv(c12,s12));wait'';
+
+end
+
 game:service'RunService'.Stepped:connect(function()
 b2.Anchored=true
 b2.Parent,block.Parent=bill,bill
@@ -42,19 +56,7 @@ end)
 
 wait(4)
 block.Anchored=true
-local CLerp=function(p,cf2,v)
 
-        local st,cf1=tick'',p.CFrame;local c1,c2,c3,c4,c5,c6,c7,c8,c9,c10,c11,c12=cf1:components'';
-
-        local s1,s2,s3,s4,s5,s6,s7,s8,s9,s10,s11,s12=cf2:components'';
-
-        local glv=function(v1,v2)return v1+(v2-v1)*v*(1+tick''-st);end;
-
-        p.CFrame=CFrame.new(glv(c1,s1),glv(c2,s2),glv(c3,s3),glv(c4,s4),glv(c5,s5)
-
-        ,glv(c6,s6),glv(c7,s7),glv(c8,s8),glv(c9,s9),glv(c10,s10),glv(c11,s11),glv(c12,s12));wait'';
-
-end
 
 while wait(0) do block.BrickColor=BrickColor.new'New Yeller'
         b2.BrickColor=block.BrickColor
