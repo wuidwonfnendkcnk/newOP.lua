df42c04d103c884a9a10e7ccc011f096238d0f50
islandmakerepic <mysandwich00@gmail.com>
2016-03-16 18:16:25 -0700

    Update bill.lua

--- bill.lua
+++ bill.lua
@@ -15,7 +15,7 @@ local socket=eye:clone()
 socket.Parent=eye
 socket:clearAllChildren()
 socket.CanCollide,eye.CanCollide=false,false
-socket.Size=Vector3.new(1.175,4,2.35)
+socket.Size=Vector3.new(1.175,4,0.75)
 eye.BrickColor=BrickColor.White()
 socket.BrickColor=BrickColor.new'Really black'
 local hat=Instance.new("Model",bill)
